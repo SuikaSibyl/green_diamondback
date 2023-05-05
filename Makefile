ede427dddd2499da81aac9e42dfa141c7a91e5f9
Mark Barbone <mark.l.barbone@gmail.com>
2023-05-05 09:54:41 -0700

    Add infra for running a reference interpreter

--- Makefile
+++ Makefile
@@ -15,10 +15,20 @@ tests/%.run: tests/%.s runtime/start.rs
 	ar rcs tests/lib$*.a tests/$*.o
 	rustc -L tests/ -lour_code:$* runtime/start.rs -o tests/$*.run
 
-.PHONY: test
+.PHONY: test test-reference-interpreter clean
 test:
 	cargo build
 	cargo test
+
+target/reference-interpreter: diamondback.ml
+	ocamlopt diamondback.ml -o target/reference-interpreter
+	rm diamondback.{cmx,cmi,o}
+
+test-reference-interpreter: target/reference-interpreter
+	[ -z "$(git status --porcelain)" ] || ( echo 'Ensure your git working directory is clean first' ; exit 1 )
+	cp reference_interpreter_shim.rs tests/infra/mod.rs
+	cargo test
+	git checkout tests/infra/mod.rs
 
 clean:
 	rm -f tests/*.a tests/*.s tests/*.run tests/*.o
---
+++ reference_interpreter_shim.rs
@@ -0,0 +1,106 @@
+use std::{path::Path, process::Command};
+
+pub(crate) enum TestKind {
+    Success,
+    RuntimeError,
+    StaticError,
+}
+
+#[macro_export]
+macro_rules! success_tests {
+    ($($tt:tt)*) => { $crate::tests!(Success => $($tt)*); }
+}
+
+#[macro_export]
+macro_rules! runtime_error_tests {
+    ($($tt:tt)*) => { $crate::tests!(RuntimeError => $($tt)*); }
+}
+
+#[macro_export]
+macro_rules! static_error_tests {
+    ($($tt:tt)*) => { $crate::tests!(StaticError => $($tt)*); }
+}
+
+#[macro_export]
+macro_rules! tests {
+    ($kind:ident =>
+        $(
+            {
+                name: $name:ident,
+                file: $file:literal,
+                $(input: $input:literal,)?
+                expected: $expected:literal $(,)?
+                $(" $(tt:$tt)* ")?
+            }
+        ),*
+        $(,)?
+    ) => {
+        $(
+            #[test]
+            fn $name() {
+                #[allow(unused_assignments, unused_mut)]
+                let mut input = None;
+                $(input = Some($input);)?
+                let kind = $crate::infra::TestKind::$kind;
+                $crate::infra::run_test(stringify!($name), $file, input, $expected, kind);
+            }
+        )*
+    };
+}
+
+pub(crate) fn run_test(
+    name: &str,
+    file: &str,
+    input: Option<&str>,
+    expected: &str,
+    kind: TestKind,
+) {
+    let file = Path::new("tests").join(file);
+    match kind {
+        TestKind::Success => run_success_test(name, &file, expected, input),
+        TestKind::RuntimeError => run_runtime_error_test(name, &file, expected, input),
+        TestKind::StaticError => run_static_error_test(name, &file, expected),
+    }
+}
+
+fn run_success_test(_name: &str, file: &Path, expected: &str, input: Option<&str>) {
+    let actual_output = run_interpreter(file, input).unwrap();
+    diff(expected, &actual_output);
+}
+
+fn run_runtime_error_test(_name: &str, file: &Path, _expected: &str, input: Option<&str>) {
+    let err = run_interpreter(file, input).unwrap_err();
+    assert!(err.contains("Type") || err.contains("Overflow"));
+}
+
+fn run_static_error_test(_name: &str, file: &Path, _expected: &str) {
+    let err = run_interpreter(file, None).unwrap_err();
+    assert!(err.contains("Syntax") || err.contains("BadProgram"));
+}
+
+fn run_interpreter(snek_file: &Path, input: Option<&str>) -> Result<String, String> {
+    let output = Command::new("./target/reference-interpreter")
+        .arg(snek_file)
+        .arg(input.unwrap_or("false"))
+        .output()
+        .expect("could not run the interpreter");
+    if output.status.success() {
+        Ok(String::from_utf8(output.stdout).unwrap())
+    } else {
+        Err(String::from_utf8(output.stderr).unwrap())
+    }
+}
+
+fn diff(expected: &str, found: &str) {
+    let expected = expected.trim();
+
+    let expected_lines: Vec<&str> = expected.lines().collect();
+    let actual_lines: Vec<&str> = found.lines().collect();
+    if expected_lines != actual_lines {
+        eprintln!(
+            "output differed!\n{}",
+            prettydiff::diff_lines(found, expected)
+        );
+        panic!("test failed");
+    }
+}
